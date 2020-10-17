1be9353f31ce816627859ec1fc7f8d029096cdae
Fadi EL HADDAD <fadi.haddad.dev@gmail.com>
2020-10-17 13:30:35 +0100

    adding ru gaps for fibonnacci and centered master layouts

--- fibonacci.c
+++ fibonacci.c
@@ -6,54 +6,61 @@ fibonacci(Monitor *mon, int s) {
 	for(n = 0, c = nexttiled(mon->clients); c; c = nexttiled(c->next), n++);
 	if(n == 0)
 		return;
-	
-	nx = mon->wx;
-	ny = 0;
-	nw = mon->ww;
-	nh = mon->wh;
-	
-	for(i = 0, c = nexttiled(mon->clients); c; c = nexttiled(c->next)) {
-		if((i % 2 && nh / 2 > 2 * c->bw)
-		   || (!(i % 2) && nw / 2 > 2 * c->bw)) {
-			if(i < n - 1) {
-				if(i % 2)
-					nh /= 2;
+	if (n == 1) {
+		c = nexttiled(mon->clients);
+		resize(c, mon->wx, mon->wy, mon->ww - 2 * c->bw, mon->wh - 2 * c->bw, 0);
+		return;
+	}
+
+	nx = mon->wx + mon->gappx;
+	ny = mon->gappx;
+	nw = mon->ww - 2*mon->gappx;
+	nh = mon->wh - 2*mon->gappx;
+
+	for (i = 0, c = nexttiled(mon->clients); c; c = nexttiled(c->next)) {
+		if ((i % 2 && nh / 2 > 2*c->bw)
+		   || (!(i % 2) && nw / 2 > 2*c->bw)) {
+			if (i < n - 1) {
+				if (i % 2)
+					nh = (nh - mon->gappx) / 2;
 				else
-					nw /= 2;
-				if((i % 4) == 2 && !s)
-					nx += nw;
-				else if((i % 4) == 3 && !s)
-					ny += nh;
+					nw = (nw - mon->gappx) / 2;
+
+				if ((i % 4) == 2 && !s)
+					nx += nw + mon->gappx;
+				else if ((i % 4) == 3 && !s)
+					ny += nh + mon->gappx;
 			}
-			if((i % 4) == 0) {
-				if(s)
-					ny += nh;
+			if ((i % 4) == 0) {
+				if (s)
+					ny += nh + mon->gappx;
 				else
-					ny -= nh;
+					ny -= nh + mon->gappx;
 			}
-			else if((i % 4) == 1)
-				nx += nw;
-			else if((i % 4) == 2)
-				ny += nh;
-			else if((i % 4) == 3) {
-				if(s)
-					nx += nw;
+			else if ((i % 4) == 1)
+				nx += nw + mon->gappx;
+			else if ((i % 4) == 2)
+				ny += nh + mon->gappx;
+			else if ((i % 4) == 3) {
+				if (s)
+					nx += nw + mon->gappx;
 				else
-					nx -= nw;
+					nx -= nw + mon->gappx;
 			}
-			if(i == 0)
-			{
-				if(n != 1)
-					nw = mon->ww * mon->mfact;
-				ny = mon->wy;
+			if (i == 0)	{
+				if (n != 1)
+					nw = (mon->ww - 2*mon->gappx - mon->gappx) * mon->mfact;
+				ny = mon->wy + mon->gappx;
 			}
-			else if(i == 1)
-				nw = mon->ww - nw;
+			else if (i == 1)
+				nw = mon->ww - nw - mon->gappx - 2*mon->gappx;
 			i++;
 		}
-		resize(c, nx, ny, nw - 2 * c->bw, nh - 2 * c->bw, False);
+
+		resize(c, nx, ny, nw - (2*c->bw), nh - (2*c->bw), False);
 	}
 }
+
 
 void
 dwindle(Monitor *mon) {
